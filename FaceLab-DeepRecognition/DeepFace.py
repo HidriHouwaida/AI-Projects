c4f1a0a9f5819bff02c904d9662d8620b51cc77a
Houwaida Hidri <houwaidahidri@gmail.com>
2025-07-17 17:28:03 +0200

    Update DeepFace.py

--- FaceLab-DeepRecognition/DeepFace.py
+++ FaceLab-DeepRecognition/DeepFace.py
@@ -11,3 +11,11 @@ Recherche = DeepFace.find(
   Albert = 'C:/Users/PC/Desktop/FaceRecognition/Pictures/Known/Albert.png',
   Dossier_De_Recherche = 'C:/Users/PC/Desktop/FaceRecognition/Pictures/Unknown/',
 )
+print(Recherche)
+
+# Analyse démographique et émotionnelle
+Detaction_ED = DeepFace.analyze(
+  img_path = 'C:/Users/PC/Desktop/FaceRecognition/Pictures/Unknown/garcon_riant.jpg', 
+  actions = ['age', 'gender', 'race', 'emotion'],
+)
+print(Detaction_ED)
