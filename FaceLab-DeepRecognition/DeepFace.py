e81b201735a77ec840d0741b151d6f93ea1adab1
Houwaida Hidri <houwaidahidri@gmail.com>
2025-07-17 17:23:21 +0200

    Update DeepFace.py

--- FaceLab-DeepRecognition/DeepFace.py
+++ FaceLab-DeepRecognition/DeepFace.py
@@ -1,1 +1,13 @@
+from deepface import DeepFace
+# Comparaison des visages dans deux images 
+Comparaison = DeepFace.verify(
+  Albert = 'C:/Users/PC/Desktop/FaceRecognition/Pictures/Known/Albert.png',
+  Erwin = 'C:/Users/PC/Desktop/FaceRecognition/Pictures/Unknown/Erwin.jpg',
+)
+print(Comparaison)
 
+# Recherche des images semblable à une image spécifique dans un dossier 
+Recherche = DeepFace.find(
+  Albert = 'C:/Users/PC/Desktop/FaceRecognition/Pictures/Known/Albert.png',
+  Dossier_De_Recherche = 'C:/Users/PC/Desktop/FaceRecognition/Pictures/Unknown/',
+)
